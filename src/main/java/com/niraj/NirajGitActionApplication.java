ddaa05bb9e82a813863749c862a7db47b2ec7c5d
Niraj <niraj.webtech@gmail.com>
2022-09-02 00:33:06 +0530

    First Commit

---
+++ src/main/java/com/niraj/NirajGitActionApplication.java
@@ -0,0 +1,13 @@
+package com.niraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirajGitActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirajGitActionApplication.class, args);
+	}
+
+}
